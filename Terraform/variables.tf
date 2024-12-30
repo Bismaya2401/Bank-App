f996b976b1db390f57b17104b8b2895cea09d15b
Bismaya2401 <bismaya_2401res44@iitp.ac.in>
2024-12-30 12:10:39 +0530

    Add GitOps and Jenkinsfile

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -10,7 +10,7 @@ variable "ami_id" {
 
 variable "instance_type" {
   description = "Instance type for the EC2 instance"
-  default     = "t2.micro"
+  default     = "t2.large"
 }
 
 variable "my_enviroment" {
